b380cfe6c22697ffe6ba74b70637ec6512ab18b6
Rulx Philomé Alexis <rulxphilome.alexis@gmail.com>
2024-03-14 00:48:00 +0100

    Update values files and adding loki

--- init.sh
+++ init.sh
@@ -5,4 +5,10 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo update
 
 echo "---- Deploying KubePrometheusStack with specific values's file"
-helm install kps prometheus-community/kube-prometheus-stack --create-namespace  -n monitoring -f ./values-kps.yaml
+helm upgrade --install kps prometheus-community/kube-prometheus-stack --create-namespace  -n monitoring -f ./values-kps.yaml
+
+echo "---- Deploying Loki"
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+helm upgrade --install kps-loki grafana/loki-stack --create-namespace  -n monitoring -f ./values-kps-loki.yaml
